0ed67eab3f618f6d57756056ac82a0f2589d2c0a
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-07 15:58:25 +0530

    cloudinary setup to handle image upload

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDatabse from "./lib/db.js";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRouter);
