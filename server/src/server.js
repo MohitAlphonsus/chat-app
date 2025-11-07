d9999313b2e58109a8aa4cb05787de9ca72d6418
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-07 23:18:24 +0530

    cors origin defined

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
 app.use("/api/auth", authRouter);
