a818474eb04b9969c77e6c5c85168906b89ed3e0
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-08 23:18:57 +0530

    check auth error resolved

--- server/src/server.js
+++ server/src/server.js
@@ -12,9 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
 app.use("/api/auth", authRouter);
