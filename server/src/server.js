d849665d6cf519351583cfb6900b36db7d1118b2
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-12-04 11:17:12 +0530

    path changed

--- server/src/server.js
+++ server/src/server.js
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/dist")));
+	app.use(express.static(path.join(__dirname, "../client/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
 	});
 }
 
