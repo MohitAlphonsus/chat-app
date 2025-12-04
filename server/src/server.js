455b3f5a516ad6fdb3cc94d788b9d63c08d81a10
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-12-04 10:47:45 +0530

    final setup for project s completed

--- server/src/server.js
+++ server/src/server.js
@@ -5,15 +5,24 @@ import connectDatabse from "./lib/db.js";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
+}
 
 // Routes
 app.use("/api/auth", authRouter);
